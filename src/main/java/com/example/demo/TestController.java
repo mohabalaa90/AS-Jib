7322297b802cf22f278a8d742c3005da91d92aa6
Mohab <mohab.ashoor@qeema.net>
2023-05-26 02:46:57 +0300

    init

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+
+    @GetMapping(path = "/service")
+    public String getService(){
+        return "Hello From Service !";
+    }
+}
