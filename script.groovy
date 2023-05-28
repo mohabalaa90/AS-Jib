0591b42038b0dd997553d26bd6bf3cdd3a17662c
Mohab <mohab.ashoor@qeema.net>
2023-05-28 03:16:31 +0300

    lastest

--- script.groovy
+++ script.groovy
@@ -52,7 +52,7 @@ def deploytok8s(){
   echo "deploying to kubernetes...................."
   sh '''if ! ssh root@192.168.1.100 kubectl get secrets | grep qeema-secret
     then
-      withCredentials\(\[usernamePassword\(credentialsId:qeemaReg-Credentials , passwordVariable:PASS , usernameVariable:USER\)\]\){
+      withCredentials(\[usernamePassword(credentialsId:qeemaReg-Credentials , passwordVariable:PASS , usernameVariable:USER)\]){
       ssh root@192.168.1.100 kubectl create secret docker-registry qeema-secret --docker-server=https://registry.tools.idp.qeema.io --docker-username=$USER --docker-password=$PASS 
         }
     else
